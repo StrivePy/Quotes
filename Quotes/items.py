4e4fc2f64c9499385422fcdee0f513899e829758
StrivePy <1013974267@qq.com>
2018-05-23 01:26:09 +0800

    submit dev branch

--- Quotes/items.py
+++ Quotes/items.py
@@ -9,8 +9,6 @@ import scrapy
 
 
 class QuotesItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
     text = scrapy.Field()
     author = scrapy.Field()
     tags = scrapy.Field()
